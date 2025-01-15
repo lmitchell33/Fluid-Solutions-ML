ac0bcff7741d2ccc3acfb167768d5c95d9436575
Lucas Mitchell <lhmitchell1333@gmail.com>
2025-01-15 12:58:14 -0500

    data updates

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -3,6 +3,11 @@ import torch
 
 class DataSet(torch.utils.data.Dataset):
     '''Wrapper class used to preprocess the training data'''
+    # TODO: Update this class based on the data we want to use
 
     def __init__(self):
-        super().__init__()
+        super(DataSet, self).__init__()
+
+
+    def __getitem__(self, index):
+        return super().__getitem__(index)
